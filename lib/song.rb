591135c06e6278aaeadea78d834107d4397dc93c
Christopher Osborn <z68ozz@gmail.com>
2020-03-13 23:57:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,7 @@ class Song
     end
    
     def self.new_from_filename(name)
-         song_array = name.split("-")
+         song_array = name.split(" - ")
          song_array[1] = song_array[1].chomp(".mp3")
          song = self.new
          song.name = song_array[1]
