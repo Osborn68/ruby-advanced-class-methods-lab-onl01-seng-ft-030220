59d6a33eaaf11680a716cfb1631c2e2a813b228d
Christopher Osborn <z68ozz@gmail.com>
2020-03-13 22:48:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,6 +36,12 @@ class Song
    nil 
  end
    
+   def self.find_or_create_by_name(name)
+     if self.find_by_name(name)
+       self.find_by_name(name)
+     else
+       self.create_by_name(name)
+     end
      
    end 
  
